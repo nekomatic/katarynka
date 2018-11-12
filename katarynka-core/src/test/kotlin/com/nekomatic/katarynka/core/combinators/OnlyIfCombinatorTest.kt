9cdd100008ad1440ecc2c31e6491621f6deb2935
nekomatic <mike@nekomatic.com>
2018-11-12 23:05:30 +0000

    tweaks to the OnlyIf test

--- katarynka-core/src/test/kotlin/com/nekomatic/katarynka/core/combinators/OnlyIfCombinatorTest.kt
+++ katarynka-core/src/test/kotlin/com/nekomatic/katarynka/core/combinators/OnlyIfCombinatorTest.kt
@@ -3,6 +3,7 @@ package com.nekomatic.katarynka.core.combinators
 import arrow.core.Either
 import com.nekomatic.katarynka.core.result.Failure
 import com.nekomatic.katarynka.core.input.Input
+import com.nekomatic.katarynka.core.input.LineInput
 import com.nekomatic.katarynka.core.result.Success
 import com.nekomatic.katarynka.core.parsers.MatchParser
 import org.junit.jupiter.api.Test
@@ -11,15 +12,15 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 
 internal class OnlyIfCombinatorTest {
-    private val textEmpty = "".toList()
-    private val text0 = "0".toList()
-    private val text1 = "1".toList()
-    private val parser = MatchParser<Char, Input<Char>>("non-zero digit") { it.isDigit() } onlyIf { it != '0' }
+    private val textEmpty = ""
+    private val text0 = "0"
+    private val text1 = "1"
+    private val parser = MatchParser<Char, LineInput<Char>>("non-zero digit") { it.isDigit() } onlyIf { it != '0' }
 
     @DisplayName("Empty input")
     @Test
     fun emptyInput() {
-        val input = Input.create(textEmpty.iterator())
+        val input = LineInput.create(textEmpty.iterator())
         val result = parser.parse(input)
         org.junit.jupiter.api.assertAll(
                 {
@@ -27,11 +28,11 @@ internal class OnlyIfCombinatorTest {
                     { "OnlyIf parser result of an empty input should be Either.Left" }
                 },
                 {
-                    assertEquals((result as Either.Left<Failure<Char, Input<Char>>>).a.expected, "non-zero digit")
+                    assertEquals("non-zero digit", (result as Either.Left<Failure<Char, LineInput<Char>>>).a.expected)
                     { "Expected of a failed OnlyIf parser should be the value of parser's rename" }
                 },
                 {
-                    assertEquals((result as Either.Left<Failure<Char, Input<Char>>>).a.remainingInput.position, result.a.failedAtInput.position)
+                    assertEquals((result as Either.Left<Failure<Char, LineInput<Char>>>).a.remainingInput.position, result.a.failedAtInput.position)
                     { "Remaining input of a failed OnlyIf parser should be at the same position as the starting input" }
                 }
         )
@@ -42,7 +43,7 @@ internal class OnlyIfCombinatorTest {
     @DisplayName("Mathing input")
     @Test
     fun matchingInput() {
-        val input = Input.create(text1.iterator())
+        val input = LineInput.create(text1.iterator())
         val result = parser.parse(input)
         org.junit.jupiter.api.assertAll(
                 {
@@ -50,11 +51,11 @@ internal class OnlyIfCombinatorTest {
                     { "OnlyIf parser result of a matching input should be Either.Right" }
                 },
                 {
-                    assertEquals('1', (result as Either.Right<Success<Char, Input<Char>, Char>>).b.value)
+                    assertEquals('1', (result as Either.Right<Success<Char, LineInput<Char>, Char>>).b.value)
                     { "Value of a successful OnlyIf parser should be equal to the current element" }
                 },
                 {
-                    assertEquals(input.position + 1, (result as Either.Right<Success<Char, Input<Char>, Char>>).b.remainingInput.position)
+                    assertEquals(input.position + 1, (result as Either.Right<Success<Char, LineInput<Char>, Char>>).b.remainingInput.position)
                     { "Position of the remaining input of a successful ItemParser should be at larger by 1 than the starting input's position" }
                 }
         )
@@ -63,7 +64,7 @@ internal class OnlyIfCombinatorTest {
     @DisplayName("Non-matching input")
     @Test
     fun nonMatchingInput() {
-        val input = Input.create(text0.iterator())
+        val input = LineInput.create(text0.iterator())
         val result = parser.parse(input)
         org.junit.jupiter.api.assertAll(
                 {
@@ -71,11 +72,11 @@ internal class OnlyIfCombinatorTest {
                     { "OnlyIf parser result of a non-matching input should be Either.Left" }
                 },
                 {
-                    assertEquals((result as Either.Left<Failure<Char, Input<Char>>>).a.expected, "non-zero digit")
+                    assertEquals("non-zero digit", (result as Either.Left<Failure<Char, LineInput<Char>>>).a.expected)
                     { "Expected of a failed OnlyIf parser should be the value of parser's rename" }
                 },
                 {
-                    assertEquals(input.position, (result as Either.Left<Failure<Char, Input<Char>>>).a.remainingInput.position)
+                    assertEquals(input.position, (result as Either.Left<Failure<Char, LineInput<Char>>>).a.remainingInput.position)
                     { "Remaining input of a failed OnlyIf parser should be at the same position as the starting input" }
                 }
         )
