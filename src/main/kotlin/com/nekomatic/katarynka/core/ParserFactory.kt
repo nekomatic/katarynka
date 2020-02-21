e79ae3289cc68911608962b90706070dab0a3376
Michal Pawluk <mike@nekomatic.com>
2020-02-21 09:52:45 +0000

    added successful parsser with default value in addition to Unit

--- src/main/kotlin/com/nekomatic/katarynka/core/ParserFactory.kt
+++ src/main/kotlin/com/nekomatic/katarynka/core/ParserFactory.kt
@@ -23,76 +23,86 @@ val p = Builder(ParserFactory<Char, LineInput<Char>>()) {
 open class ParserFactory<TItem, TIn>(val keepPayload: Boolean = true) where TIn : IInput<TItem, TIn> {
 
     private val eof =
-            this.parser("eof") { input, n, _ ->
-                eofParserFunction(
-                        input = input,
-                        name = n,
-                        factory = this
-                )
-            }
+        this.parser("eof") { input, n, _ ->
+            eofParserFunction(
+                input = input,
+                name = n,
+                factory = this
+            )
+        }
 
-    private val successful = this.parser("success") { input, _, _ ->
+    fun successful() = this.parser("success") { input, _, _ ->
         Success(
-                value = Unit,
-                startingInput = input,
-                remainingInput = input,
-                payload = when {
-                    this.keepPayload -> listOf<TItem>().some()
-                    else -> None
-                }
+            value = Unit,
+            startingInput = input,
+            remainingInput = input,
+            payload = when {
+                this.keepPayload -> listOf<TItem>().some()
+                else -> None
+            }
+        ).right()
+    }
+
+    fun <TVal : Any> successful(const: TVal) = this.parser("success") { input, _, _ ->
+        Success(
+            value = const,
+            startingInput = input,
+            remainingInput = input,
+            payload = when {
+                this.keepPayload -> listOf<TItem>().some()
+                else -> None
+            }
         ).right()
     }
 
     private val any =
-            this.any("any")
+        this.any("any")
 
     private fun anyFunction(input: TIn, n: String, fact: ParserFactory<TItem, TIn>) =
-            standardParserFunction(
-                    input = input,
-                    name = n,
-                    match = { true },
-                    factory = this
-            )
+        standardParserFunction(
+            input = input,
+            name = n,
+            match = { true },
+            factory = this
+        )
 
     fun <TVal> parser(name: String, parserFunction: ParserFunction<TItem, TIn, TVal>) =
-            Parser(
-                    name = name,
-                    factory = this,
-                    parserFunction = parserFunction
-            )
+        Parser(
+            name = name,
+            factory = this,
+            parserFunction = parserFunction
+        )
 
     fun <TVal> failing(name: String) =
-            parser<TVal>("eof") { input, _, _ ->
-                NonEmptyList.of(Failure(name, input)).left()
-            }
+        parser<TVal>("eof") { input, _, _ ->
+            NonEmptyList.of(Failure(name, input)).left()
+        }
 
     fun match(name: String, match: (TItem) -> Boolean): Parser<TItem, TIn, TItem> {
         fun f(input: TIn, n: String, fact: ParserFactory<TItem, TIn>) =
-                standardParserFunction(
-                        input = input,
-                        name = n,
-                        match = match,
-                        factory = this
-                )
+            standardParserFunction(
+                input = input,
+                name = n,
+                match = match,
+                factory = this
+            )
         return this.parser(name, ::f)
     }
 
     fun item(name: String, item: TItem): Parser<TItem, TIn, TItem> {
         fun f(input: TIn, n: String, fact: ParserFactory<TItem, TIn>) =
-                standardParserFunction(
-                        input = input,
-                        name = n,
-                        match = { it == item },
-                        factory = this
-                )
+            standardParserFunction(
+                input = input,
+                name = n,
+                match = { it == item },
+                factory = this
+            )
         return this.parser(name, ::f)
     }
 
     fun item(item: TItem) = this.item(item.toString(), item)
 
     fun any(name: String) = this.parser(name, ::anyFunction)
-
-    fun successful(): Parser<TItem, TIn, Unit> = successful
 
     fun any() = any
 
